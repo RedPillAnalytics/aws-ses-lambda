d5ce9d50914a85246b8eb0b2fd0d2c67bdb8d6c1
Kitto Khrangtong <kitto.khr@gmail.com>
2020-09-02 16:51:16 -0400

    add: comments for potential improvements

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const aws = require('aws-sdk');
-const ses = new aws.SES({region: 'us-east-1'});
 const nodemailer = require("nodemailer");
 const fetch = require('node-fetch')
 
+const ses = new aws.SES({region: 'us-east-1'});
+
 exports.handler = (event, context, callback) => {
-  // Direct URL to the raw mailOptions JSON file
+  // Direct URL to the raw mailOptions JSON file 
+  // Could/should store URL in AWS environment variables for each individual function
   let url = "https://raw.githubusercontent.com/redpillanalytics/email-repo/master/mailOptions.json" // Can store this in Lambda function env variables
 
   let settings = {method: 'GET'}
 
   // GET mailOptions from repo and construct the email from those options
+  // Could do some error checking in this area using the event/context return values
+  //   could compare against the file uploaded to S3 to the name of the repo to ensure
+  //   we get the right file sent
   fetch(url, settings).then(response => {
     const {from, subject, html, to, filename, path} = response.body
 
